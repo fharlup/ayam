904dbc092bbac896b453dc064b43110198db5e56
fharlup <35958282+fharlup@users.noreply.github.com>
2021-02-20 00:39:18 +0700

    otw1

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ sepeda_html=client.read()
 page_soup=soup(sepeda_html,"html.parser")
 client.close()
 semua=page_soup.findAll("div",{"class":"IKo3_"})
-awalprice=page_soup.findAll("span",{"class":"_89yzn"})
-price=awalprice[0].text
-
+for i in semua:
+    awalprice = page_soup.findAll("span", {"class": "_89yzn"})
+    price = awalprice[0].text
+    awalharga = page_soup.findAll("span", {"class": "_2tW1I"})
+    harga = awalharga[0].text
+    awaltempat = page_soup.findAll("span", {"class": "tjgMj"})
+    tempat = awaltempat[0].text
+    print(price)
+    print(harga)
+    print(tempat)
