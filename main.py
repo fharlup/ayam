e75e8d2cbb2b990a593edf5a70225e37bc5b74c6
fharlup <35958282+fharlup@users.noreply.github.com>
2021-02-19 20:15:13 +0700

    tes

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import bs4
 from urllib.request import urlopen as ureq
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
+urlsepeda="https://www.olx.co.id/items/q-frame-sepeda"
+client=ureq(urlsepeda)
+sepeda_html=client.read()
+client.close()
+page_soup=soup(sepeda_html,"html.parser")
+page_soup.h3
