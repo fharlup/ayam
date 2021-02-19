642be372168581b1b3ea1ea23e363fd2d0198e17
fharlup <35958282+fharlup@users.noreply.github.com>
2021-02-20 00:12:12 +0700

    HAHAHAI

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup as soup
 urlsepeda="https://www.olx.co.id/items/q-frame-sepeda"
 client=ureq(urlsepeda)
 sepeda_html=client.read()
-
 page_soup=soup(sepeda_html,"html.parser")
 client.close()
+semua=page_soup.findAll("div",{"class":"IKo3_"})
+awalprice=page_soup.findAll("span",{"class":"_89yzn"})
+price=awalprice[0].text
+
