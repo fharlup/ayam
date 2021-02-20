ebe3b2c3796b56405587c8498e1531e03d6d9f5d
fharlup <35958282+fharlup@users.noreply.github.com>
2021-02-20 19:48:24 +0700

    seperempat

--- webscrap.py
+++ webscrap.py
@@ -1,23 +1,26 @@
-
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
-urlsepeda="https://www.olx.co.id/items/q-frame-sepeda"
-x=0
-i=0
-client=ureq(urlsepeda)
-sepeda_html=client.read()
-page_soup=soup(sepeda_html,"html.parser")
+import itertools
+urlsepeda = "https://www.olx.co.id/items/q-frame-sepeda"
+result={}
+client = ureq(urlsepeda)
+sepeda_html = client.read()
+page_soup = soup(sepeda_html, "html.parser")
 client.close()
-semua=page_soup.findAll("div",{"class":"IKo3_"})
+semua = page_soup.findAll("ul", {"class": "rl3f9 _3mXOU"})
+
 awalprice = page_soup.findAll("span", {"class": "_89yzn"})
-price = awalprice[x].text
-awalharga = page_soup.findAll("span", {"class": "_2tW1I"})
-harga = awalharga[0].text
+price = awalprice[0].text
+nama = page_soup.findAll("span", {"class": "_2tW1I"})
+atasnama = nama[0].text
 awaltempat = page_soup.findAll("span", {"class": "tjgMj"})
 tempat = awaltempat[0].text
-totalp=len(awalprice)
-while i==totalp:
 
-    x=x+1
-    print(price)
+for (a,b,c)in zip(nama,awalprice,awaltempat):
+    print(a.text,b.text,c.text)
+
+
+
+
+
 
